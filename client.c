8e77f66b07aac97c08efefca8f84c1ca4fcab509
itsgurpreetsingh <79744977+itsgurpreetsingh@users.noreply.github.com>
2022-11-14 01:06:02 +0530

    Add files via upload

---
+++ client.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+	
+#define PORT 1234
+#define MAXLINE 1024
+	
+int main() {
+        int broadcast=1;
+	int sockfd;
+	char buffer[MAXLINE];
+	struct sockaddr_in addr , senderaddr;
+		
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+		
+	memset(&addr, 0, sizeof(addr));
+    	memset(&addr, 0, sizeof(senderaddr));
+		
+	addr.sin_family = AF_INET; 
+	addr.sin_addr.s_addr = inet_addr("192.168.64.255");
+	addr.sin_port = htons(PORT);
+	setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&broadcast,sizeof(broadcast));	
+	if ( bind(sockfd, (const struct sockaddr *)&addr,
+			sizeof(addr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+		
+	int len, n;
+	
+	len = sizeof(senderaddr); 
+	
+	n = recvfrom(sockfd, (char *)buffer, MAXLINE,MSG_WAITALL, ( struct sockaddr *) &senderaddr,(socklen_t *)&len);
+	buffer[n] = '\0';
+	printf("Message Received : %s\n", buffer);
+
+    close(sockfd);
+		
+	return 0;
+}
